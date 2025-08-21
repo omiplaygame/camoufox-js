ece28baadb4d0e810e3e83202877c364d293dfe5
OmiPlay <xassomi@gmail.com>
2025-08-21 18:40:05 +0300

    publicIp

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Camoufox, NewBrowser } from './sync_api.js';
 export { type LaunchOptions, launchOptions } from './utils.js';
 export { launchServer } from './server.js';
+export { publicIP } from './ip.js';
